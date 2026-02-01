a0f1f523cadd13a787bc6148a8ea18ecb57581a5
Abhradip Roy <abhra0312@gmail.com>
2026-02-01 22:53:42 +0530

    commented out login confirm

--- src/app/(main)/(auth)/login/actions.ts
+++ src/app/(main)/(auth)/login/actions.ts
@@ -1,23 +1,23 @@
-'use server'
+// 'use server'
 
-import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
+// import { revalidatePath } from 'next/cache'
+// import { redirect } from 'next/navigation'
 
-import { createClient } from '@/app/utils/supabase/server'
+// import { createClient } from '@/app/utils/supabase/server'
 
-const supabase = await createClient()
+// const supabase = await createClient()
 
 
-export async function signInWithEmail(formData: FormData) {
-    const email = formData.get('email') as string
-    const { data, error } = await supabase.auth.signInWithOtp({
-      email: email,
-      options: {
-        // set this to false if you do not want the user to be automatically signed up
-        shouldCreateUser: true,
-        emailRedirectTo: 'http://localhost:3000/auth/confirm',
-      },
-    })
-    console.log('data', data)
-    console.log('error', error)
-  }
+// export async function signInWithEmail(formData: FormData) {
+//     const email = formData.get('email') as string
+//     const { data, error } = await supabase.auth.signInWithOtp({
+//       email: email,
+//       options: {
+//         // set this to false if you do not want the user to be automatically signed up
+//         shouldCreateUser: true,
+//         emailRedirectTo: 'http://localhost:3000/auth/confirm',
+//       },
+//     })
+//     console.log('data', data)
+//     console.log('error', error)
+//   }
--- src/app/(main)/(auth)/login/page.tsx
+++ src/app/(main)/(auth)/login/page.tsx
@@ -1,13 +1,13 @@
-import { signInWithEmail } from './actions'
+// import { signInWithEmail } from './actions'
 
-export default function LoginPage() {
-  return (
-    <form className="flex flex-col gap-4">
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
+// export default function LoginPage() {
+//   return (
+//     <form className="flex flex-col gap-4">
+//       <label htmlFor="email">Email:</label>
+//       <input id="email" name="email" type="email" required />
      
-      <button formAction={signInWithEmail}>Log in</button>
-      <button formAction={signInWithEmail}>Sign up</button>
-    </form>
-  )
-}
+//       <button formAction={signInWithEmail}>Log in</button>
+//       <button formAction={signInWithEmail}>Sign up</button>
+//     </form>
+//   )
+// }
