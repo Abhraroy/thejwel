205da63c025423589f214a229bde6727aec766fd
Abhradip Roy <abhra0312@gmail.com>
2026-03-13 14:53:13 +0530

    Removed HelpIcon from Navbar component and its associated menu item for a cleaner navigation experience.

--- src/components/NavbarUI/Navbar.tsx
+++ src/components/NavbarUI/Navbar.tsx
@@ -159,23 +159,6 @@ const OrdersIcon = ({ className = 'w-5 h-5' }: IconProps) => (
   </svg>
 );
 
-const HelpIcon = ({ className = 'w-5 h-5' }: IconProps) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={2}
-    stroke="currentColor"
-    className={className}
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
-    />
-  </svg>
-);
-
 const AboutIcon = ({ className = 'w-5 h-5' }: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -360,12 +343,6 @@ export default function Navbar({ cartCount = 0, isAuthenticated = false, onCartC
       href: '/orders',
       icon: <OrdersIcon />,
       requiresAuth: true,
-    },
-    {
-      label: 'Help & Support',
-      href: '/help',
-      icon: <HelpIcon />,
-      requiresAuth: false,
     },
     {
       label: 'About',
